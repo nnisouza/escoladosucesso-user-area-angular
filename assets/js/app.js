062d5f6fbaaf0c27a0208426a9fd98e02be41a44
Giovanni <giov@nnisouza.com>
2015-04-15 10:07:56 -0300

    update, please

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,9 @@ angular.module('userArea', ['ngRoute', 'mm.foundation', 'ngAnimate'])
 .controller('userAreaCtrl', function ($scope, $http) {
     $scope.cart = [];
     $http.get('info.json').success(function (response) {
-        $scope.courses = response;
+        $scope.courses = response.cursos[0].doUsuario;
 
-        console.log(response);
+        console.log($scope.courses.title);
     });
 //    $scope.addToCart = function (product) {
 //        var found = false;
@@ -39,4 +39,4 @@ angular.module('userArea', ['ngRoute', 'mm.foundation', 'ngAnimate'])
 //            }
 //        });
 //    };
-})
+});
