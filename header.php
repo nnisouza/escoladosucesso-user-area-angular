f9fbd2b288165f5721e8e4575b5f50581c07edb2
Giovanni <giov@nnisouza.com>
2015-04-15 03:41:20 -0300

    initial commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+include '../../db_config/db_config.php';
+?>
+
+<!DOCTYPE html>
+	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Escola Online | Cursos</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="assets/css/normalize.min.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/remodal.css">
+
+        <script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
+
