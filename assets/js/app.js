d4e7a0e73d9f009310f9cab423f50d67ecfe4739
Giovanni <giov@nnisouza.com>
2015-04-16 14:09:35 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,35 +17,4 @@ angular.module('userArea', ['ngRoute', 'mm.foundation', 'ngAnimate'])
         $scope.modal.push(curso);
         console.log(curso);
     }
-//    $scope.addToCart = function (product) {
-//        var found = false;
-//        $scope.cart.forEach(function (item) {
-//            if (item.id === product.id) {
-//                item.quantity++;
-//                found = true;
-//            }
-//        });
-//        if (!found) {
-//            $scope.cart.push(angular.extend({quantity: 1}, product));
-//        }
-//    };
-//    $scope.getCartPrice = function () {
-//        var deliveryPrice = parseInt($('#deliveryPrice').val());
-//        var total = deliveryPrice;
-//        $scope.cart.forEach(function (product) {
-//            total += product.price * product.quantity;
-//        });
-//        return total;
-//    };
-//    $scope.removeFromCart = function (product, cart) {
-//        $scope.cart.forEach(function (item) {
-//            if (item.id === product.id) {
-//                if (item.quantity > 1){
-//                    item.quantity--;
-//                } else {
-//                    cart.splice(cart.indexOf(item), 1);
-//                }
-//            }
-//        });
-//    };
 });
